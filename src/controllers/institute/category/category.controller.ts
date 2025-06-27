2f91c9ef7a93acfc1f0f65dd7eccceb52a8a2a6e
Roshan Gurung <roshc10grg@gmail.com>
2025-06-27 13:28:07 +0545

    get all categories function added

--- src/controllers/institute/category/category.controller.ts
+++ src/controllers/institute/category/category.controller.ts
@@ -1,9 +1,10 @@
-import { Response } from "express";
+import { response, Response } from "express";
 import asyncHandler from "../../../utils/asyncHandler";
 import { IExtedREquest } from "../../../utils/types";
 import { categoryValidation } from "../../../database/validation/instCategory/category.validation";
 import sequelize from "../../../database/connection";
 import ApiResponse from "../../../utils/ApiResponse";
+import { QueryTypes } from "sequelize";
 
 
 const insertCategory = asyncHandler(async(req: IExtedREquest, res: Response)=>{
@@ -11,12 +12,20 @@ const insertCategory = asyncHandler(async(req: IExtedREquest, res: Response)=>{
       const validData =  categoryValidation.parse(req.body);
 
       await sequelize.query(`INSERT INTO category_${instituteNumber}(categoryName,categoryDescription) VALUES (?,?)`,{
+         type: QueryTypes.INSERT,
         replacements:[
             validData.categoryName,validData.categoryDescription
         ]
       });
 return res.status(200).json(new ApiResponse(200,"category Inserted Successfully"))
-       
 });
-export {insertCategory};
+
+const getAllCategory = asyncHandler(async(req: IExtedREquest,res: Response)=>{
+    const instituteNumber = req.user?.currentInstituteNumber
+    const categories=   await sequelize.query(`SELECT * FROM category_${instituteNumber}`,{
+        type: QueryTypes.SELECT
+      });
+      return res.status(200).json(new ApiResponse(200,categories,"Caregories Fetched Successfully."))
+});
+export {insertCategory, getAllCategory};
 
--- src/routes/institute/category/category.routes.ts
+++ src/routes/institute/category/category.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import isLoggedIn from "../../../middleware/isLoggedin";
-import { insertCategory } from "../../../controllers/institute/category/category.controller";
+import { insertCategory,getAllCategory } from "../../../controllers/institute/category/category.controller";
 
 export const instCategoryRouter = Router();
 
 instCategoryRouter.route("/category/create").post(isLoggedIn,insertCategory);
+instCategoryRouter.route("/category/getCategories").get(isLoggedIn,getAllCategory);
 
