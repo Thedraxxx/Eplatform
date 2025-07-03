9784f9c67aca68de4e086be31848310ec1152366
Roshan Gurung <roshc10grg@gmail.com>
2025-07-03 20:21:34 +0545

    dynamic token generation with teacher contorller

--- src/controllers/teacher/teacher.controller.ts
+++ src/controllers/teacher/teacher.controller.ts
@@ -1,15 +1,17 @@
 import { Response } from "express";
 import asyncHandler from "../../utils/asyncHandler";
 import { IExtedREquest } from "../../utils/types";
-import { ITeacher, teacherValidation } from "../../database/validation/teacher/login";
+import { teacherValidation } from "../../database/validation/teacher/login";
 import sequelize from "../../database/connection";
 import ApiError from "../../utils/APIerror";
 import { QueryTypes } from "sequelize";
 import bcrypt from "bcrypt"
 import ApiResponse from "../../utils/ApiResponse";
+import generateToken from "../../utils/generateToken";
 interface ITeacehr {
     teacherPassword: string
-    teacherid: string
+    id: string
+    password: string
 }
 
 const teacherController= asyncHandler(async(req:IExtedREquest,res:Response)=>{
@@ -21,7 +23,7 @@ const teacherController= asyncHandler(async(req:IExtedREquest,res:Response)=>{
         throw new ApiError(401,"Unauthorized instittue number");
       }
       //email check ...
-      const teacherData: ITeacher[] = await sequelize.query(`SELECT teacherEmail, teacherPassword FROM teacher_${instituteNumber} WHERE teacherEmail=?`,{
+      const teacherData: ITeacehr[]  = await sequelize.query(`SELECT id, teacherEmail, teacherPassword FROM teacher_${instituteNumber} WHERE teacherEmail=?`,{
         type: QueryTypes.SELECT,
         replacements: [validTeacherData.email]
       })
@@ -33,10 +35,8 @@ const teacherController= asyncHandler(async(req:IExtedREquest,res:Response)=>{
     if(!isMatched){
         throw new ApiError(400,"password doesnt match")
     }
-    const data = {
-        teacherData[0].
-    }
-    generateToken()
+    
+    const token = await generateToken({id: teacherData[0].id, instituteNumber: validTeacherData.instituteNumber})
 
      return res.json(new ApiResponse(200,"Welcome to dashboard sir"));
 
--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,11 +1,11 @@
   import jwt from "jsonwebtoken";
-
-
-  const generateToken = async(data)=>{
+  import { envconfig } from "../config/config";
+  import {StringValue} from "ms"
+  
+  const generateToken = async(data: {id: string, instituteNumber ?: string})=>{
         return jwt.sign(
-            {
-               
-            }
+                 data,envconfig.accessToken_expiry as string,{expiresIn: envconfig.accessToken_expiry as StringValue}
         )
   }
  
+  export default generateToken
